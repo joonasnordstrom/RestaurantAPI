17015ce5c9fa50951bbeb42d2f8659f75ed36c06
joonasnordstrom <joonas.nordstrom12@gmail.com>
2017-10-01 14:52:50 +0300

    Add project files.

---
+++ RestaurantAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantAPI.Startup))]
+namespace RestaurantAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
