074dad770888909c5e501339b3c939cf08641bad
Joni Saira <joni.k.saira@gmail.com>
2017-10-07 13:51:07 +0300

    Dokkarisivun toimivuus valmiina, css aloitettu

---
+++ RestaurantAPI/Models/DocumentItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantAPI.Models
+{
+    public class DocumentItem
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ExampleRequest { get; set; }
+        public string Information { get; set; }
+    }
+
+}
